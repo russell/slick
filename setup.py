19e9f8d47ae0722c74b6070a8cff39f1d35678ff
Russell Sim <russell.sim@arcs.org.au>
2009-07-09 11:04:54 +1000

    updated readme and prepared to add it to pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '1.0'
 
 setup(name='slick',
       version=version,
-      description="Client tools for slcs",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="Commandline tool for generating and signing a SWITCH SLCS certificate.",
+      long_description=open(os.path.join("README")).read() + "\n" +
+                       open(os.path.join("CHANGES")).read(),
+      classifiers=[
+        "Topic :: Security :: Cryptography",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Topic :: System :: Distributed Computing",
+        "Intended Audience :: End Users/Desktop",
+        "Environment :: Console",
+        "Topic :: Security",
+        ],
       keywords='',
       author='Russell Sim',
       author_email='russell.sim@arcs.org.au',
-      url='',
+      url='http://code.arcs.org.au/gitorious/shibboleth/slick',
+      download_url='http://code.arcs.org.au/pypi/slick/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-          'M2Crypto',
           'arcs.gsi',
       ],
       entry_points="""
